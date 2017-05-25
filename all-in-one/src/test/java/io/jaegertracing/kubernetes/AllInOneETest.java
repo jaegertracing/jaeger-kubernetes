2eae62c2a157658ede4b5103fc5c9fdb9244205d
Pavol Loffay <p.loffay@gmail.com>
2017-05-25 12:30:18 +0200

    All-in-one template (#2)
    * All-in-one template
    
    * Add maven wrapper
    
    * Fix link in readme
    
    * travis dist trusty
    
    * travis maven cache
    
    * e2e profile
    
    * Fail if k8s is not configured
    
    * fix typo kubetnetes

---
+++ all-in-one/src/test/java/io/jaegertracing/kubernetes/AllInOneETest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2017 The Jaeger Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.jaegertracing.kubernetes;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.arquillian.cube.kubernetes.annotations.Named;
+import org.arquillian.cube.kubernetes.annotations.PortForward;
+import org.arquillian.cube.requirement.ArquillianConditionalRunner;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.fabric8.kubernetes.api.model.v2_2.Service;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @author Pavol Loffay
+ */
+@RunWith(ArquillianConditionalRunner.class)
+public class AllInOneETest {
+    private static final String SERVICE_NAME = "jaeger-all-in-one";
+
+    private OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .build();
+
+    @Named(SERVICE_NAME)
+    @ArquillianResource
+    private Service jaegerService;
+
+    @Named(SERVICE_NAME)
+    @PortForward
+    @ArquillianResource
+    private URL jaegerUiUrl;
+
+    @Test
+    public void testUiResponds() throws IOException, InterruptedException {
+        Request request = new Request.Builder()
+                .url(jaegerUiUrl)
+                .get()
+                .build();
+
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            Assert.assertEquals(200, response.code());
+        }
+    }
+}
